46b09feae510d78b1b80e18462a26118f64dfc11
Thong <117352680+Th0ngpv@users.noreply.github.com>
2024-04-03 23:40:41 +0700

    change food position when the snake eat it

--- script.js
+++ script.js
@@ -4,22 +4,11 @@ let foodX = 13, foodY = 10;
 let snakeX = 5, SnakeY = 10;
 let velocityX = 0,velocityY = 0;
 
+//generate a random food position
 const changeFoodPosition = () => {
     //create an random X and Y coordination from 1-30 for food
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
-}
-
-const initGame = () => {
-    //create the food element
-    let htmlMarkup = `<div class="food" style = "grid-area: ${foodX} / ${foodY}"></div>`;
-    //update the snake's head position base on the velocity
-    snakeX += velocityX;
-    SnakeY += velocityY; 
-    //create the snake head element
-    htmlMarkup += `<div id="snake" style ="grid-area: ${snakeX}/${SnakeY}"></div>`;
-    playBoard.innerHTML = htmlMarkup;
-    
 }
 
 //change the direction of the snake base on arrow key
@@ -38,6 +27,25 @@ const changeDirection = (e) => {
         velocityY = 0;
     }
 }
+
+const initGame = () => {
+    //check if the snake head and the food are at the same position
+    if (snakeX === foodX && SnakeY === foodY) {
+        changeFoodPosition();
+    }
+    //create the food element
+    let htmlMarkup = `<div class="food" style = "grid-area: ${foodX} / ${foodY}"></div>`;
+    
+    //update the snake's head position base on the velocity
+    snakeX += velocityX;
+    SnakeY += velocityY; 
+    //create the snake head element
+    htmlMarkup += `<div id="snake" style ="grid-area: ${snakeX}/${SnakeY}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
+    
+}
+
+
 
 
 changeFoodPosition();
